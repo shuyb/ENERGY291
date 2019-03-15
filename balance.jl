d500479d30d96e6b248af7a490dbd497bd1369e2
miaoyf13 <miaoyf13@hotmail.com>
2019-03-15 13:24:15 -0700

    Save file
    Update string to int

--- balance.jl
+++ balance.jl
@@ -1,41 +1,45 @@
-loadpath = "flow"
-flowProfiles = Dict()
-for each in readdir(loadpath)
-    println("working on $each")
-    # original file
-    temp = CSV.read(joinpath(loadpath, each), type = [String, Int, Int])
-    deleterows!(temp, 1994)
-    temp = convert(Matrix, temp[:,2:3])
-    flowProfiles[each[1:4]] = temp
+function load_balance(generationProfiles,loadProfiles,countrylist)
+    loadpath = "flow"
+    flowProfiles = Dict()
+    for each in readdir(loadpath)
+        println("working on $each")
+        # original file
+        temp = CSV.read(joinpath(loadpath, each), type = [String, Int, Int])
+        deleterows!(temp, 1994)
+        temp = convert(Matrix, temp[:,2:3])
+        flowProfiles[each[1:4]] = temp
+        print(temp)
+    end
+    flowProfiles["ESMA"] = sum(generationProfiles["Spain"], dims = 2) - loadProfiles["Spain"] - flowProfiles["ESFR"][:,2] - flowProfiles["ESPT"][:,2] + flowProfiles["ESFR"][:,1] + flowProfiles["ESPT"][:,1]
+
+    EUload = zeros(8760)
+    EUgen = zeros(8760)
+    for i = 1:length(countrylist)
+        EUload += loadProfiles[countrylist[i]]
+        EUgen += sum(generationProfiles[countrylist[i]], dims = 2)
+    end
+
+    println(EUgen - EUload - flowProfiles["ESMA"] - flowProfiles["FIRU"][:,2] - flowProfiles["EERU"][:,2] - flowProfiles["LVRU"][:,2] - flowProfiles["LTRU"][:,2] - flowProfiles["LTBY"][:,2] - flowProfiles["PLUA"][:,2] - flowProfiles["SKUA"][:,2] - flowProfiles["HUUA"][:,2] - flowProfiles["ROUA"][:,2] - flowProfiles["BGTR"][:,2] - flowProfiles["GRTR"][:,2] - flowProfiles["GRAL"][:,2] - flowProfiles["RSAL"][:,2] - flowProfiles["MEAL"][:,2] - flowProfiles["SIHR"][:,2] - flowProfiles["BAHR"][:,2] - flowProfiles["RSHR"][:,2] - flowProfiles["HSHR"][:,2] + flowProfiles["FIRU"][:,1] + flowProfiles["EERU"][:,1] + flowProfiles["LVRU"][:,1] + flowProfiles["LTRU"][:,1] + flowProfiles["LTBY"][:,1] + flowProfiles["PLUA"][:,1] + flowProfiles["SKUA"][:,1] + flowProfiles["HUUA"][:,1] + flowProfiles["ROUA"][:,1] + flowProfiles["BGTR"][:,1] + flowProfiles["GRTR"][:,1] + flowProfiles["GRAL"][:,1] + flowProfiles["RSAL"][:,1] + flowProfiles["MEAL"][:,1] + flowProfiles["SIHR"][:,1] + flowProfiles["BAHR"][:,1] + flowProfiles["RSHR"][:,1] + flowProfiles["HSHR"][:,1])
+
+    return flowProfiles
+    # ESMA
+    # FIRU
+    # NORU
+    # EERU
+    # LVRU
+    # LTRU
+    # LTBY
+    # PLUA
+    # SKUA
+    # HUUA
+    # ROUA
+    # BGTR
+    # GRTR
+    # GRAL
+    # RSAL
+    # MEAL
+    # SIHR
+    # BAHR
+    # RSHR
+    # HUHR
 end
-flowProfiles["ESMA"] = sum(generationProfiles["Spain"], dims = 2) - loadProfiles["Spain"] - flowProfiles["ESFR"][:,2] - flowProfiles["ESPT"][:,2] + flowProfiles["ESFR"][:,1] + flowProfiles["ESPT"][:,1]
-
-EUload = zeros(8760)
-EUgen = zeros(8760)
-for i = 1:length(countrylist)
-    global EUload = EUload + loadProfiles[countrylist[i]]
-    global EUgen = EUgen + sum(generationProfiles[countrylist[i]], dims = 2)
-end
-
-println(EUgen - EUload - flowProfiles["ESMA"] - flowProfiles["FIRU"][:,2] - flowProfiles["EERU"][:,2] - flowProfiles["LVRU"][:,2] - flowProfiles["LTRU"][:,2] - flowProfiles["LTBY"][:,2] - flowProfiles["PLUA"][:,2] - flowProfiles["SKUA"][:,2] - flowProfiles["HUUA"][:,2] - flowProfiles["ROUA"][:,2] - flowProfiles["BGTR"][:,2] - flowProfiles["GRTR"][:,2] - flowProfiles["GRAL"][:,2] - flowProfiles["RSAL"][:,2] - flowProfiles["MEAL"][:,2] - flowProfiles["SIHR"][:,2] - flowProfiles["BAHR"][:,2] - flowProfiles["RSHR"][:,2] - flowProfiles["HSHR"][:,2] + flowProfiles["FIRU"][:,1] + flowProfiles["EERU"][:,1] + flowProfiles["LVRU"][:,1] + flowProfiles["LTRU"][:,1] + flowProfiles["LTBY"][:,1] + flowProfiles["PLUA"][:,1] + flowProfiles["SKUA"][:,1] + flowProfiles["HUUA"][:,1] + flowProfiles["ROUA"][:,1] + flowProfiles["BGTR"][:,1] + flowProfiles["GRTR"][:,1] + flowProfiles["GRAL"][:,1] + flowProfiles["RSAL"][:,1] + flowProfiles["MEAL"][:,1] + flowProfiles["SIHR"][:,1] + flowProfiles["BAHR"][:,1] + flowProfiles["RSHR"][:,1] + flowProfiles["HSHR"][:,1])
-
-# ESMA
-# FIRU
-# NORU
-# EERU
-# LVRU
-# LTRU
-# LTBY
-# PLUA
-# SKUA
-# HUUA
-# ROUA
-# BGTR
-# GRTR
-# GRAL
-# RSAL
-# MEAL
-# SIHR
-# BAHR
-# RSHR
-# HUHR
--- proj_yufei.jl
+++ proj_yufei.jl
@@ -8,37 +8,38 @@ include("offset.jl")
 include("transmission.jl")
 include("opt.jl")
 include("max_penetration.jl")
+include("balance.jl")
 
 abbreviation = loadabbr("abbr.csv")
 loadProfiles = loadload("2018", abbreviation)
 countrylist = sort(collect(keys(loadProfiles)))
-generationProfiles = loadgeneration("generation_0312", abbreviation)
+generationProfiles = loadgeneration("generation", abbreviation)
 offset_status = 2 #0 standard #1:offset all but solar #2:offset only solar #offset all
 if offset_status == 0
-    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_GMT1_0312.csv", countrylist)
+    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_GMT1.csv", countrylist)
     for each in countrylist
         generationProfiles[each] = rescale_generation(generationProfiles, solarProfiles, each, true)
     end
 elseif offset_status == 1
-    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_GMT1_0312.csv", countrylist)
+    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_GMT1.csv", countrylist)
     offset!(loadProfiles, generationProfiles, "peakadjusment.csv")
     for each in countrylist
         generationProfiles[each] = rescale_generation(generationProfiles, solarProfiles, each, true)
     end
 elseif offset_status == 2
-    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_Hypo_0312.csv", countrylist)
+    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_Hypo.csv", countrylist)
     for each in countrylist
         generationProfiles[each] = rescale_generation(generationProfiles, solarProfiles, each, true)
     end
 elseif offset_status == 3
-    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_Hypo_0312.csv", countrylist)
+    solarProfiles = loadsolar("2015EuropeSolarHourlyMW_Hypo.csv", countrylist)
     offset!(loadProfiles, generationProfiles, "peakadjusment.csv")
     for each in countrylist
         generationProfiles[each] = rescale_generation(generationProfiles, solarProfiles, each, true)
     end
 end
 
-transmission_matrix = loadtransmission("transmission_0312.csv", countrylist)
+transmission_matrix = loadtransmission("transmission.csv", countrylist)
 loadRescaled = rescale_load(loadProfiles,countrylist,generationProfiles)
 
 nStep = 8760
